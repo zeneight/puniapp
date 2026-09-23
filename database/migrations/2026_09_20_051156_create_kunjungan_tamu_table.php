10a518d229e02f025356ae7fcf75a01d8b59348a
zeneight <thezeneight@gmail.com>
2026-09-23 13:38:07 +0800

    feat: menambahkan migrate dan beberapa file pendukung dan informasi lainnya

--- database/migrations/2026_09_20_051156_create_kunjungan_tamu_table.php
+++ database/migrations/2026_09_20_051156_create_kunjungan_tamu_table.php
@@ -33,6 +33,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('kunjungan_tamu');
+        Schema::dropIfExists('detail_kunjungan_tamu');
     }
 };
